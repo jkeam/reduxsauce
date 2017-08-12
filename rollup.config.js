e713782d631f5ef15cd03b373055bb41c8f08545
Steve Kellock <steve@kellock.ca>
2017-08-12 13:24:13 -0400

    Fixes some build issues.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,29 @@
 import babel from 'rollup-plugin-babel'
 import ramda from 'rollup-plugin-ramda'
+import filesize from 'rollup-plugin-filesize'
+
+const ramdaFns = [
+  'isNil',
+  'is',
+  'has',
+  'pipe',
+  'trim',
+  'merge',
+  'split',
+  'without',
+  'map',
+  'fromPairs',
+  'anyPass',
+  'isEmpty',
+  'curry',
+  'join',
+  'keys',
+  'mapObjIndexed',
+  'replace',
+  'toUpper',
+  '__',
+  'zipObj'
+].map(fn => `ramda/src/${fn}`)
 
 export default {
   entry: 'lib/reduxsauce.js',
@@ -7,14 +31,12 @@ export default {
   plugins: [
     babel({
       babelrc: false,
-      presets: [
-        ['es2015', { modules: false }],
-        'stage-0'
-      ],
-      plugins: ['external-helpers'],
+      presets: [['es2015', { modules: false }], 'stage-0'],
+      plugins: ['external-helpers']
     }),
     ramda(),
+    filesize()
   ],
-  external: ['ramda', 'ramdasauce'],
+  external: [...ramdaFns, 'ramdasauce'],
   dest: 'dist/reduxsauce.js'
 }
