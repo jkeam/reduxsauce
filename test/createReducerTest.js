0627f2232aebf06d2e3651d1e46f8c64c3b353f8
Jonathan Keam <jpkeam@gmail.com>
2019-10-21 00:32:51 -0400

    Feature/upgrade dependencies (#81)
    * removed unused dep and upgraded various dependencies
    
    * fixed linting issues
    
    * update repo url

--- test/createReducerTest.js
+++ test/createReducerTest.js
@@ -16,6 +16,7 @@ test('throws an error when the handlers are not an object', (t) => {
 test('throws an error when the handlers have an undefined key', (t) => {
   const a = (state, action) => state
   t.throws(() => createReducer(1, { [undefined]: a }))
+  // eslint-disable-next-line no-void
   t.throws(() => createReducer(1, { [void 0]: a }))
 })
 
@@ -25,21 +26,21 @@ test('creates a reducer function', (t) => {
 
 test('dodges the wrong actions', (t) => {
   const a = (state, action) => state + 1
-  const r = createReducer(0, { 'hi': a })
-  const v = r(5, {type: 'wrong'})
+  const r = createReducer(0, { hi: a })
+  const v = r(5, { type: 'wrong' })
   t.is(v, 5)
 })
 
 test('dodges null actions', (t) => {
   const a = (state, action) => state + 1
-  const r = createReducer(0, { 'hi': a })
+  const r = createReducer(0, { hi: a })
   const v = r(5, null)
   t.is(v, 5)
 })
 
 test('dodges actions with no type', (t) => {
   const a = (state, action) => state + 1
-  const r = createReducer(0, { 'hi': a })
+  const r = createReducer(0, { hi: a })
   const v = r(5, { bad: 'type' })
   t.is(v, 5)
 })
@@ -47,8 +48,8 @@ test('dodges actions with no type', (t) => {
 test('invokes the correct actions', (t) => {
   const i = 5
   const a = (state, action) => state + 1
-  const r = createReducer(i, { 'hi': a })
-  const v = r(i, {type: 'hi'})
+  const r = createReducer(i, { hi: a })
+  const v = r(i, { type: 'hi' })
   t.is(v, 6)
 })
 
@@ -56,17 +57,17 @@ test('falls down to default handler', (t) => {
   const i = 5
   const a = (state, action) => state + 1
   const b = (state, action) => state + 2
-  const r = createReducer(i, { 'hi': a, [Types.DEFAULT]: b })
-  const v1 = r(i, {type: 'hi'})
+  const r = createReducer(i, { hi: a, [Types.DEFAULT]: b })
+  const v1 = r(i, { type: 'hi' })
   t.is(v1, 6)
-  const v2 = r(i, {type: 'unknown action'})
+  const v2 = r(i, { type: 'unknown action' })
   t.is(v2, 7)
 })
 
 test('invokes the correct action on an object', (t) => {
   const i = { i: 5 }
   const a = (state, action) => ({ i: state.i + 1 })
-  const r = createReducer(i, { 'hi': a })
+  const r = createReducer(i, { hi: a })
   const v = r(i, { type: 'hi' })
   t.deepEqual(v, { i: 6 })
 })
@@ -74,10 +75,10 @@ test('invokes the correct action on an object', (t) => {
 test('allows a reducer with null initial state', (t) => {
   const i = null
   const a = (state, action) => 'hello'
-  const r = createReducer(i, { 'hi': a })
-  const v = r(i, {type: 'unknown action'})
+  const r = createReducer(i, { hi: a })
+  const v = r(i, { type: 'unknown action' })
   t.is(v, null)
-  const v2 = r(i, {type: 'hi'})
+  const v2 = r(i, { type: 'hi' })
   t.is(v2, 'hello')
 })
 
